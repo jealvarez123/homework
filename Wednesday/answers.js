ddade9ef6459f574eaf7c238b007ff7a83d91dae
jealvarez123 <jealvarez123@gmail.com>
2018-03-21 20:55:55 -0500

    Commit 4 - Fizz Buzz answered

--- Wednesday/answers.js
+++ Wednesday/answers.js
@@ -16,14 +16,32 @@ console.log("Can I kick it?");
 //
 // }
 //
-var catThoughts = ["...human...why you taking pictures of me?..", "...the catnip made me do it...", "...why does the red dot always get away..."]
+// var catThoughts = ["...human...why you taking pictures of me?..", "...the catnip made me do it...", "...why does the red dot always get away..."]
+//
+// console.log(catThoughts);
+//
+// for (var i = 0; i <= 20; i++) {
+//   if (i % 2===0){
+//     var random = catThoughts[Math.floor(Math.random() * catThoughts.length)];
+//     console.log(random);
+//   }
+//
+//
+// }
 
-console.log(catThoughts);
+for (var i = 1; i <=100 ; i++) {
+  if (i % 3===0 && i % 5 ===0) {
+    console.log("fizzbuzz");
 
-for (var i = 0; i <= 20; i++) {
-  if (i % 2===0){
-    var random = catThoughts[Math.floor(Math.random() * catThoughts.length)];
-    console.log(random);
+
+  } else if (i % 5 ===0 ) {
+    console.log("buzz");
+
+  } else if (i % 3 === 0 ) {
+    console.log("fizz");
+
+  }  else {
+    console.log(i);
   }
 
 
