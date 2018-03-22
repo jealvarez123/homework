bb2dcbcda634621da01e5102fc64b1cff1d63d0b
jealvarez123 <jealvarez123@gmail.com>
2018-03-21 20:43:57 -0500

    Commit 3 - Excited Kittens answered

--- Wednesday/answers.js
+++ Wednesday/answers.js
@@ -1,9 +1,30 @@
-for (var i = 1; i <= 20; i++) {
-console.log(i);
-}
+console.log("Can I kick it?");
 
-for (var i = 0; i <= 200; i++) {
-if ( i % 2 === 0 ){
-  console.log(i);
-}
+// for (var i = 1; i <= 20; i++) {
+// console.log(i);
+// }
+//
+// for (var i = 0; i <= 200; i++) {
+// if ( i % 2 === 0 ){
+//   console.log(i);
+// }
+// }
+//
+// for (var i = 0; i <= 20; i++) {
+//
+// console.log("Love me, pet me! HSSSSSS!");
+//
+// }
+//
+var catThoughts = ["...human...why you taking pictures of me?..", "...the catnip made me do it...", "...why does the red dot always get away..."]
+
+console.log(catThoughts);
+
+for (var i = 0; i <= 20; i++) {
+  if (i % 2===0){
+    var random = catThoughts[Math.floor(Math.random() * catThoughts.length)];
+    console.log(random);
+  }
+
+
 }
